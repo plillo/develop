03bc56e00df70928a98239528c0e105ef8958a09
Paolo Lillo <paolo.lillo@hash.it>
2016-04-14 15:09:24 +0200

    pull

---
+++ it.hash.osgi.application/src/it/hash/osgi/application/service/ApplicationManager.java
@@ -0,0 +1,10 @@
+package it.hash.osgi.application.service;
+
+import java.util.List;
+
+import it.hash.osgi.user.attribute.Attribute;
+
+
+public interface ApplicationManager {
+	public void filterAttributes(String appCode, List<Attribute> attributes);
+}
