607722d16d3d4a85c8aa6a538b1e8190fdcb3a4a
Anto71 <montinariantonella@gmail.com>
2016-06-23 10:31:10 +0200

    Update PromotionService.java

--- it.hash.osgi.business.promotion/src/it/hash/osgi/business/promotion/service/PromotionService.java
+++ it.hash.osgi.business.promotion/src/it/hash/osgi/business/promotion/service/PromotionService.java
@@ -8,33 +8,38 @@ import it.hash.osgi.business.promotion.Promotion;
 
 
 public interface PromotionService {
-	Map<String, Object> getBusiness(Map<String, Object> pars);
 	
 	Map<String, Object> createPromotion(Promotion promotion);
 	Map<String, Object> createPromotion(Map<String, Object> pars);
-	Map<String, Object> updatePromotion(String uuid, Promotion promotion);
-	Map<String, Object> updatePromotion(String uuid, Map<String, Object> pars);
-	Map<String, Object> deletePromotion(String uuid);
-	Map<String, Object> updatePromotionLogo(String uuid, String type, byte[] encodeBase64);
-	Map<String, Object> updatePromotionLogo(String uuid, String type, InputStream istream);
+	
+
 	List<Promotion> retrievePromotion(String search);
 	List<Promotion> retrievepromotions(String criterion, String search);
 	List<Promotion> getPromotions();
 	Promotion getPromotion(String uuid);
 	Promotion getPromotion(String uuid, boolean withLogo);
-     Map<String, Object> getPromotion(Map<String, Object> pars) ;
-		
+    Map<String, Object> getPromotion(Map<String, Object> pars) ;
+	List<Promotion> getPromotionsByBusinessUuid(String businessUuid);
+     
+	Map<String, Object> updatePromotion(Promotion promotion);
+	Map<String, Object> updatePromotion(String uuid, Map<String, Object> pars);
+	Map<String, Object> updatePromotionLogo(String uuid, String type, byte[] encodeBase64);
+	Map<String, Object> updatePromotionLogo(String uuid, String type, InputStream istream);
 	
-	Promotion getPromotionByBusinessFiscalCode(String BusinessFiscalCode);
+	
+	Map<String, Object> deletePromotion(String uuid,String type);
+	
+	
+//	Promotion getPromotionByBusinessFiscalCode(String BusinessFiscalCode);
 //	Promotion getPromotionByBusinessFiscalCode(String BusinessFiscalCode, boolean withLogo);
-	Promotion getPromotionByBusinessPartitaIva(String partitaIva);
+//	Promotion getPromotionByBusinessPartitaIva(String partitaIva);
 //	Promotion getPromotionByBusinessPartitaIva(String partitaIva, boolean withLogo);
-	Promotion getPromotionByBusinessName(String Name);
-	Promotion getPromotionByBusinessName(String Name, boolean withLogo);
-	Promotion getPromotionByBusinessId(String businessId);
-	Promotion getPromotionByBusinessId(String businessId, boolean withLogo);
-	Promotion getPromotionByBusinessUuid(String uuid);
-	Promotion getPromotionByBusinessUuid(String uuid, boolean withLogo);
+//	Promotion getPromotionByBusinessName(String Name);
+//	Promotion getPromotionByBusinessName(String Name, boolean withLogo);
+//	Promotion getPromotionByBusinessId(String businessId);
+	//Promotion getPromotionByBusinessId(String businessId, boolean withLogo);
+
+//	Promotion getPromotionByBusinessUuid(String uuid, boolean withLogo);
 	
 	
 	
